21884b9cc9cace08b4fb0636c4e13e692178ed21
SmithyySHU <c0033267@hallam.shu.ac.uk>
2024-06-25 20:55:39 +0100

    Start on user registration

--- webapps/webapps/settings.py
+++ webapps/webapps/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webreporting.apps.WebreportingConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- webapps/webreporting/views.py
+++ webapps/webreporting/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -10,3 +12,15 @@ def about(request) :
 
 def contact(request) :
     return render (request, 'webreporting/contact.html', {'title': 'Contact JobFinders'})
+
+def register(request) :
+    if request.method == 'POST' :
+        form = UserCreationForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect ('webreporting:home')
+    else :
+        form = UserCreationForm()
+    return render (request, 'users/register.html', {'form': form, 'title': 'Register for JobFinders'})
