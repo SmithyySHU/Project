1f220bc589a0ddf0c8be78851e8bc5e7af3875c6
SmithyySHU <c0033267@hallam.shu.ac.uk>
2024-06-25 20:16:50 +0100

    Basic Django site

--- webapps/webreporting/views.py
+++ webapps/webreporting/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request) :
+    return render (request, 'webreporting/home.html')
+
+def about(request) :
+    return render (request, 'webreporting/about.html')
+
+def contact(request) :
+    return render (request, 'webreporting/contact.html')
