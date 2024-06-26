4bf2fac98f8d4866474721256ea6ef7ed65d3aa1
SmithyySHU <c0033267@hallam.shu.ac.uk>
2024-06-27 00:01:58 +0100

    Login working - User profiles can be created (basic)

--- webapps/webapps/settings.py
+++ webapps/webapps/settings.py
@@ -130,3 +130,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'webreporting:home'
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
