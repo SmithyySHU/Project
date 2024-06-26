a239611ba5b58916df0675a6deacd79f3c170708
SmithyySHU <c0033267@hallam.shu.ac.uk>
2024-06-27 00:33:17 +0100

    Profile Page w/ update profile features

--- webapps/users/views.py
+++ webapps/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 
@@ -19,5 +20,21 @@ def register(request) :
     return render (request, 'users/register.html', {'form': form, 'title': 'Register for JobFinders'})
 
 @login_required
-def profile(request) :
-    return render (request, 'users/profile.html', {'title' : 'User Profile'})
+def profile(request):
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    if request.method == 'POST':
+       
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+
+   
+    context = {'u_form': u_form, 'p_form': p_form, 'title': 'User Profile'}
+    return render(request, 'users/profile.html', context)
